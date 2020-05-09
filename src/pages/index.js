203f12329de0a00019432041e94bfeeb02427fb0
arjoooooon <arjun.taneja02@gmail.com>
2020-05-09 22:11:25 +0800

    Fixed scrolling issues and updated index text

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,19 +18,24 @@ const App = () => {
                     Hi there! Thank you for visiting my site. I am Arjun, a high school student at UWCSEA in Singapore. I am a curious 
                     person and I like to build things using new technology that may ultimately help people.<br/><br/>
 
-                    Some of the things I have been up to recently include co-founding the Smartbin – a sustainable recycling solution. 
-                    We were so excited when the project got featured in the Google Blog! I am currently working on moving the project 
+                    Some of the things I have been up to recently include co-founding the <a href="www.sgsmartbin.com" target="_blank"
+                    className={styles.link}>Smartbin</a> – a sustainable recycling solution. 
+                    We were so excited when the project got featured in the <a 
+                    href = "https://www.blog.google/around-the-globe/google-asia/singapore-students-using-cloud-smarter-recycling/"
+                    target="_blank" className={styles.link}>Google Blog</a>! I am currently working on moving the project 
                     forward, and introducing it to the real world.<br/><br/>
 
-                    I loved my time working with the amazing folks at the innovation lab at Palo IT, where I did my internship. I got 
-                    the opportunity to work on a live greenhouse management system in Cambodia. We built a technology to use AI to manage 
+                    I loved my time working with the amazing folks at the innovation lab at <a href="https://www.palo-it.com/"
+                    target="_blank" className={styles.link}>Palo IT</a>, where I did my internship. I got 
+                    the opportunity to work on a live <a href="https://www.youtube.com/watch?v=KVHErOIxbqw" target="_blank"
+                    className={styles.link}>greenhouse management system</a> in Cambodia. We built a technology to use AI to manage 
                     a large number of greenhouses automatically. We think this could help reduce greenhouse gas emissions and be used
                     for farming in remote areas and who knows - even on another planet?<br/><br/>
 
-                    I enjoy public speaking, writing, learning new languages and doing what I can to help people and the environment. As 
+                    I enjoy public speaking, <Link to="/blog" className={styles.link}>writing</Link>, learning new languages and doing what I can to help people and the environment. As 
                     the leader of my computer science club, I teach a class of 20 + G9-12 students in programming fundamentals in Java. My 
-                    love for languages(Hindi, Spanish and Japanese so far) inspired me to create a self learning app called the Kanji 
-                    trainer.<br/><br/>
+                    love for languages (Hindi, Spanish and Japanese so far) inspired me to create a self learning app called the <Link 
+                    to="/experience" className={styles.link}>Kanji Trainer</Link>.<br/><br/>
 
                     Thank you or reading and feel free to drop me a line at hi @arjuntaneja.com
                 </span>
