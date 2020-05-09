ef42bd724524e4becc480e0e8f66893f6ea6f49d
arjoooooon <arjun.taneja02@gmail.com>
2020-05-09 21:58:19 +0800

    Fixed horizontal scrollbar issues and updated homepage text

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,20 +15,24 @@ const App = () => {
                 <h1 className={styles.descriptionHeader}>Arjun Taneja</h1>
                 <img src={muguri} alt='face' width={115} height={115} className={styles.descriptionImage}/>
                 <span className={styles.descriptionText}>
-                Hi there! I am Arjun, a high school student at UWCSEA. I like to build things, and I'm passionate about making the world
-                a better place through technology<br /><br />
+                    Hi there! Thank you for visiting my site. I am Arjun, a high school student at UWCSEA in Singapore. I am a curious 
+                    person and I like to build things using new technology that may ultimately help people.<br/><br/>
 
-                Some of the things I have been up to recently include cofounding the <a href="http://www.sgsmartbin.com" target="_blank" className={styles.link}>Smartbin</a> – a
-                sustainable recycling solution. We were so excited when the project got featured in
-                the < a href = "https://www.blog.google/around-the-globe/google-asia/singapore-students-using-cloud-smarter-recycling/" target="_blank" className={styles.link} > Google Blog </a>! I am currently working on moving the project forward, and
-                introducing it to the real world. I also enjoyed my internship with tech firm <a href="https://www.palo-it.com/" target="_blank" className={styles.link}>Palo IT</a> where I got the opportunity to work on a <a href="https://www.youtube.com/watch?v=KVHErOIxbqw" target="_blank" className={styles.link}>greenhouse management system</a>, a technology that allows automatic, AI-powered management of greenhouses in order
-                to ultimately reduce greenhouse gas emissions. <br /><br />
+                    Some of the things I have been up to recently include co-founding the Smartbin – a sustainable recycling solution. 
+                    We were so excited when the project got featured in the Google Blog! I am currently working on moving the project 
+                    forward, and introducing it to the real world.<br/><br/>
 
-                I enjoy <Link to='/experience' className={styles.link}>public speaking</Link>, writing, learning new languages and doing what I can to help people and the environment. As the
-                leader of my computer science club, I teach a class of 20 + G9 - 12 students in programming fundamentals in Java. My love
-                for languages inspired me to create a self learning app called the <a href="#" className={styles.link}>Kanji trainer</a>.<br/><br/>
+                    I loved my time working with the amazing folks at the innovation lab at Palo IT, where I did my internship. I got 
+                    the opportunity to work on a live greenhouse management system in Cambodia. We built a technology to use AI to manage 
+                    a large number of greenhouses automatically. We think this could help reduce greenhouse gas emissions and be used
+                    for farming in remote areas and who knows - even on another planet?<br/><br/>
 
-                Thank you or reading and feel free to drop me a line at <a href='#' className={styles.link}>hi@arjuntaneja.com</a>
+                    I enjoy public speaking, writing, learning new languages and doing what I can to help people and the environment. As 
+                    the leader of my computer science club, I teach a class of 20 + G9-12 students in programming fundamentals in Java. My 
+                    love for languages(Hindi, Spanish and Japanese so far) inspired me to create a self learning app called the Kanji 
+                    trainer.<br/><br/>
+
+                    Thank you or reading and feel free to drop me a line at hi @arjuntaneja.com
                 </span>
             </div>
             <Footer />
