d5f2d0eb94fcac627f5413dfba90cad5a1c585c3
arjoooooon <arjun.taneja02@gmail.com>
2020-05-16 12:12:06 +0800

    Added active link styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,11 +6,11 @@ const Navbar = () => {
     return (
         <div className = {styles.container} >
             <ul className={styles.navContainer} >
-                <li className={styles.navItem} ><Link className={styles.link} to='/'>HOME</Link></li>
-                <li className={styles.navItem} ><Link className={styles.link} to='/experience'>EXPERIENCE</Link></li>
-                <li className={styles.navItem} ><Link className={styles.link} to='/projects'>PROJECTS</Link></li>
-                <li className={styles.navItem} ><Link className={styles.link} to='/blog'>BLOG</Link></li>
-                <li className={styles.navItem} ><Link className={styles.link} to='/contact'>GET IN TOUCH</Link></li>
+                <li className={styles.navItem} ><Link className={styles.link} activeStyle={{color: '#4990E2'}} to='/'>HOME</Link></li>
+                <li className={styles.navItem} ><Link className={styles.link} activeStyle={{color: '#4990E2'}} to='/experience'>EXPERIENCE</Link></li>
+                <li className={styles.navItem} ><Link className={styles.link} activeStyle={{color: '#4990E2'}} to='/projects'>PROJECTS</Link></li>
+                <li className={styles.navItem} ><Link className={styles.link} activeStyle={{color: '#4990E2'}} to='/blog'>BLOG</Link></li>
+                <li className={styles.navItem} ><Link className={styles.link} activeStyle={{color: '#4990E2'}} to='/contact'>GET IN TOUCH</Link></li>
             </ul>
         </div>
     );
