2d488f7766084241d8dba88f0ab6ba0122995e4c
arjoooooon <arjun.taneja02@gmail.com>
2020-05-17 16:00:53 +0800

    Application rebuild

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,17 +26,17 @@ const App = () => {
                     Hi there! Thank you for visiting my site. I am Arjun, a high school student at UWCSEA in Singapore. I am a curious 
                     person and I like to build things using new technology that may ultimately help people.<br/><br/>
 
-                    Some of the things I have been up to recently include co-founding the <a href="http://www.sgsmartbin.com" target="_blank"
+                    Some of the things I have been up to recently include co-founding the <a href="http://www.sgsmartbin.com" target="_blank" rel="noopener noreferrer"
                     className={styles.link}>Smartbin</a> – a sustainable recycling solution. 
                     We were so excited when the project got featured in the <a 
                     href = "https://www.blog.google/around-the-globe/google-asia/singapore-students-using-cloud-smarter-recycling/"
-                    target="_blank" className={styles.link}>Google Blog</a>! I am currently working on moving the project 
+                    target="_blank" className={styles.link} rel="noopener noreferrer">Google Blog</a>! I am currently working on moving the project 
                     forward, and introducing it to the real world.<br/><br/>
 
                     I loved my time working with the amazing folks at the innovation lab at <a href="https://www.palo-it.com/"
-                    target="_blank" className={styles.link}>Palo IT</a>, where I did my internship. I got 
+                    target="_blank" className={styles.link} rel="noopener noreferrer">Palo IT</a>, where I did my internship. I got 
                     the opportunity to work on a live <a href="https://www.youtube.com/watch?v=KVHErOIxbqw" target="_blank"
-                    className={styles.link}>greenhouse management system</a> in Cambodia. We built a technology to use AI to manage 
+                    className={styles.link} rel="noopener noreferrer">greenhouse management system</a> in Cambodia. We built a technology to use AI to manage 
                     a large number of greenhouses automatically. We think this could help reduce greenhouse gas emissions and be used
                     for farming in remote areas and who knows - even on another planet?<br/><br/>
 
@@ -45,7 +45,7 @@ const App = () => {
                     love for languages (Hindi, Spanish and Japanese so far) inspired me to create a self learning app called the <Link 
                     to="/experiences" className={styles.link}>Kanji Trainer</Link>.<br/><br/>
 
-                    Thank you or reading and feel free to drop me a line at <a href="#" className={styles.link}>hello@arjuntaneja.com</a>.
+                    Thank you or reading and feel free to drop me a line at <a href="/" className={styles.link}>hello@arjuntaneja.com</a>.
                 </div>
             </div>
             <Footer />
