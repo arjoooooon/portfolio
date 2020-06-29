b8d2714235db31df33a7d097781ffa6c832afe9a
Arjun Taneja <arjuntaneja@MacBook-Pro.local>
2020-06-29 22:53:10 +0800

    minor grammar fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,8 +37,8 @@ const App = () => {
                     rel="noopener noreferrer">Maker Faire</a> and the <Link to="/experiences" className={styles.link}>Google Developer Festival</Link>. 
                     We were so excited when Google featured our innovation project on their <a 
                     href="https://www.blog.google/around-the-globe/google-asia/singapore-students-using-cloud-smarter-recycling/" target="_blank"
-                    className={styles.link} rel="noopener noreferrer">blog</a>! This year I am dedicating time to bring this project into the
-                    mainstream. Do watch this space for updates.<br/><br/>
+                    className={styles.link} rel="noopener noreferrer">blog</a>! This year I am dedicating time to make this project mainstream. 
+                    Do watch this space for updates.<br/><br/>
 
                     I loved my time working with the amazing folks at the innovation lab at <a href="https://www.palo-it.com/"
                     target="_blank" className={styles.link} rel="noopener noreferrer">Palo IT</a>, where I did my internship. I got
@@ -47,7 +47,7 @@ const App = () => {
                     technology that used AI to manage a large number of greenhouses automatically with the goal of reducing greenhouse 
                     gas emissions.<br/><br/>
 
-                    I enjoy <Link to="/experiences" className={styles.link}>public speaking</Link>, <Link to="/blog" className={styles.link}>writing</Link>, 
+                    I enjoy <Link to="/experiences" className={styles.link}>public speaking</Link>, <Link to="/blog" className={styles.link}>writing</Link> , 
                     <Link to="/experiences" className={styles.link}>learning new languages</Link> and <Link to="/experiences" className={styles.link}>
                     mentoring</Link> in my areas of expertise. As the leader of my computer science club, I teach a class of 20+ G9-12 students in 
                     programming fundamentals in Java. My love for languages (Hindi, Spanish and Japanese so far) inspired me to create a self learning app 
