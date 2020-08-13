3ff973c3d2c6d4e124d88c33b4025a60102de219
NgocDen <dndenpro@gmail.com>
2020-08-13 18:27:24 +0700

    add page project

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,64 +1,111 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
+const today = new Date().toISOString().split("T")[0]
 
-exports.createPages = async ({ graphql, actions }) => {
+module.exports = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const collection = getNode(node.parent).sourceInstanceName
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: "collection",
+      value: collection,
+    })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: `/${collection}${slug}`,
+    })
+    createNodeField({
+      node,
+      name: "isFuture",
+      value: node.frontmatter.date >= today,
+    })
+  }
+}
+module.exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions
+  const typeDefs = `
+      type MarkdownRemark implements Node {
+        frontmatter: Frontmatter
+      }
+      type Frontmatter {
+        tags: [String!]!
+      }
+    `
+  createTypes(typeDefs)
+}
+
+const COLLECTIONS = [
+  {
+    name: "blog",
+    postsPerPage: 10,
+    hasPostPage: true,
+  },
+  {
+    name: "projects",
+    postsPerPage: 10,
+    hasPostPage: true,
+  },
+]
+
+const filterEdges = name => edges =>
+  edges.filter(edge => edge.node.fields.collection === name)
+
+const buildPagesCollectionGenerator = ({ edges, createPage }) => ({
+  name,
+  postsPerPage,
+  hasPostPage,
+}) => {
+  const filteredEdges = filterEdges(name)(edges)
+
+  /**
+   * CREATE INDIVIDUAL ITEMS
+   */
+  if (hasPostPage) {
+    filteredEdges.forEach((edge, index) => {
+      const { slug } = edge.node.fields
+      const previous = index === edges.length - 1 ? null : edges[index + 1].node
+      const next = index === 0 ? null : edges[index - 1].node
+      createPage({
+        path: slug,
+        component: require.resolve(`../src/templates/${name}-post.js`),
+        context: { slug, previous, next },
+      })
+    })
+  }
+}
+
+module.exports = async function ({ actions, graphql }) {
   const { createPage } = actions
 
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  const result = await graphql(
-    `
-      {
-        allMarkdownRemark(
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-              }
+  const { data } = await graphql(`
+    query {
+      allMarkdownRemark(
+        sort: { fields: [frontmatter___date], order: DESC }
+        filter: { frontmatter: { published: { ne: false } } }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            fields {
+              collection
+              slug
+            }
+            frontmatter {
+              title
             }
           }
         }
       }
-    `
-  )
+    }
+  `)
 
-  if (result.errors) {
-    throw result.errors
-  }
-
-  // Create blog posts pages.
-  const posts = result.data.allMarkdownRemark.edges
-
-  posts.forEach((post, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node
-    const next = index === 0 ? null : posts[index - 1].node
-
-    createPage({
-      path: post.node.fields.slug,
-      component: blogPost,
-      context: {
-        slug: post.node.fields.slug,
-        previous,
-        next,
-      },
-    })
+  const pagesCollectionGenerator = buildPagesCollectionGenerator({
+    edges: data.allMarkdownRemark.edges,
+    createPage,
   })
-}
 
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
+  COLLECTIONS.forEach(pagesCollectionGenerator)
 }
--- src/components/home/SectionSkill.js
+++ src/components/home/SectionSkill.js
@@ -1,129 +1,161 @@
 import React from "react"
+import { ProgressBar } from "react-bootstrap"
 
 const SectionSkill = () => {
-    return (
-        <section className="ftco-section bg-light" id="skills-section">
-            <div className="container">
-                <div className="row justify-content-center pb-5">
-                    <div data-aos='fade-up' className="col-md-12 heading-section text-center">
-                        <span className="subheading">Skills</span>
-                        <h2 className="mb-4">My Skills</h2>
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-6 animate-box">
-                        <div data-aos='fade-up' data-aos-delay="200" className="progress-wrap">
-                            <h3>Photoshop</h3>
-                            <div className="progress">
-                                <div className="progress-bar color-1" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{ width: "75 %" }}>
-                                    <span>75%</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-6 animate-box">
-                        <div data-aos='fade-up' data-aos-delay="400" className="progress-wrap">
-                            <h3>jQuery</h3>
-                            <div className="progress">
-                                <div className="progress-bar color-2" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
-                                    <span>60%</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-6 animate-box">
-                        <div data-aos='fade-up' data-aos-delay="600" className="progress-wrap">
-                            <h3>HTML5</h3>
-                            <div className="progress">
-                                <div className="progress-bar color-3" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
-                                    <span>85%</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-6 animate-box">
-                        <div data-aos='fade-up' data-aos-delay="800" className="progress-wrap">
-                            <h3>CSS3</h3>
-                            <div className="progress">
-                                <div className="progress-bar color-4" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
-                                    <span>90%</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-6 animate-box">
-                        <div data-aos='fade-up' data-aos-delay="1000" className="progress-wrap">
-                            <h3>WordPress</h3>
-                            <div className="progress">
-                                <div className="progress-bar color-5" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
-                                    <span>70%</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-6 animate-box">
-                        <div data-aos='fade-up' data-aos-delay="1200" className="progress-wrap">
-                            <h3>SEO</h3>
-                            <div className="progress">
-                                <div className="progress-bar color-6" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
-                                    <span>80%</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="row justify-content-center py-5 mt-5">
-                    <div data-aos='fade-up' data-aos-delay="1400" className="col-md-12 heading-section text-center">
-                        <span className="subheading">What I Do</span>
-                        <h2 className="mb-4">Strategy, design and a bit of magic</h2>
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-                    </div>
-                </div>
-                <div className="row">
-                    <div data-aos='fade-up' data-aos-delay="1600" className="col-md-4 text-center d-flex">
-                        <div className="services-1">
-                            <span className="icon">
-                                <i className="flaticon-analysis"></i>
-                            </span>
-                            <div className="desc">
-                                <h3 className="mb-5"><a href="#">Explore</a></h3>
-                                <h4>Design Sprints</h4>
-                                <h4>Product Strategy</h4>
-                                <h4>UX Strategy</h4>
-                            </div>
-                        </div>
-                    </div>
-                    <div data-aos='fade-up' data-aos-delay="1800" className="col-md-4 text-center d-flex">
-                        <div className="services-1">
-                            <span className="icon">
-                                <i className="flaticon-flasks"></i>
-                            </span>
-                            <div className="desc">
-                                <h3 className="mb-5"><a href="#">Create</a></h3>
-                                <h4>Information</h4>
-                                <h4>UX/UI Design</h4>
-                                <h4>Branding</h4>
-                            </div>
-                        </div>
-                    </div>
-                    <div data-aos='fade-up' data-aos-delay="2000" className="col-md-4 text-center d-flex">
-                        <div className="services-1">
-                            <span className="icon">
-                                <i className="flaticon-ideas"></i>
-                            </span>
-                            <div className="desc">
-                                <h3 className="mb-5"><a href="#">Learn</a></h3>
-                                <h4>Prototyping</h4>
-                                <h4>User Testing</h4>
-                                <h4>UI Testing</h4>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <section className="ftco-section bg-light" id="skills-section">
+      <div className="container">
+        <div className="row justify-content-center pb-5">
+          <div
+            data-aos="fade-up"
+            className="col-md-12 heading-section text-center"
+          >
+            <span className="subheading">Skills</span>
+            <h2 className="mb-4">My Skills</h2>
+            <p>
+              Far far away, behind the word mountains, far from the countries
+              Vokalia and Consonantia
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6 animate-box">
+            <div
+              data-aos="fade-up"
+              data-aos-delay="200"
+              className="progress-wrap"
+            >
+              <h3>Photoshop</h3>
+              <ProgressBar now={75} label="75%" />
             </div>
-        </section>
-    )
+          </div>
+          <div className="col-md-6 animate-box">
+            <div
+              data-aos="fade-up"
+              data-aos-delay="400"
+              className="progress-wrap"
+            >
+              <h3>jQuery</h3>
+              <ProgressBar now={60} label="60%" />
+            </div>
+          </div>
+          <div className="col-md-6 animate-box">
+            <div
+              data-aos="fade-up"
+              data-aos-delay="600"
+              className="progress-wrap"
+            >
+              <h3>HTML5</h3>
+              <ProgressBar now={85} label="85%" />
+            </div>
+          </div>
+          <div className="col-md-6 animate-box">
+            <div
+              data-aos="fade-up"
+              data-aos-delay="800"
+              className="progress-wrap"
+            >
+              <h3>CSS3</h3>
+              <ProgressBar now={90} label="90%" />
+            </div>
+          </div>
+          <div className="col-md-6 animate-box">
+            <div
+              data-aos="fade-up"
+              data-aos-delay="1000"
+              className="progress-wrap"
+            >
+              <h3>WordPress</h3>
+              <ProgressBar now={70} label="70%" />
+            </div>
+          </div>
+          <div className="col-md-6 animate-box">
+            <div
+              data-aos="fade-up"
+              data-aos-delay="1200"
+              className="progress-wrap"
+            >
+              <h3>SEO</h3>
+              <ProgressBar now={80} label="80%" />
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-center py-5 mt-5">
+          <div
+            data-aos="fade-up"
+            data-aos-delay="1400"
+            className="col-md-12 heading-section text-center"
+          >
+            <span className="subheading">What I Do</span>
+            <h2 className="mb-4">Strategy, design and a bit of magic</h2>
+            <p>
+              Far far away, behind the word mountains, far from the countries
+              Vokalia and Consonantia
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          <div
+            data-aos="fade-up"
+            data-aos-delay="1600"
+            className="col-md-4 text-center d-flex"
+          >
+            <div className="services-1">
+              <span className="icon">
+                <i className="flaticon-analysis"></i>
+              </span>
+              <div className="desc">
+                <h3 className="mb-5">
+                  <a href="#">Explore</a>
+                </h3>
+                <h4>Design Sprints</h4>
+                <h4>Product Strategy</h4>
+                <h4>UX Strategy</h4>
+              </div>
+            </div>
+          </div>
+          <div
+            data-aos="fade-up"
+            data-aos-delay="1800"
+            className="col-md-4 text-center d-flex"
+          >
+            <div className="services-1">
+              <span className="icon">
+                <i className="flaticon-flasks"></i>
+              </span>
+              <div className="desc">
+                <h3 className="mb-5">
+                  <a href="#">Create</a>
+                </h3>
+                <h4>Information</h4>
+                <h4>UX/UI Design</h4>
+                <h4>Branding</h4>
+              </div>
+            </div>
+          </div>
+          <div
+            data-aos="fade-up"
+            data-aos-delay="2000"
+            className="col-md-4 text-center d-flex"
+          >
+            <div className="services-1">
+              <span className="icon">
+                <i className="flaticon-ideas"></i>
+              </span>
+              <div className="desc">
+                <h3 className="mb-5">
+                  <a href="#">Learn</a>
+                </h3>
+                <h4>Prototyping</h4>
+                <h4>User Testing</h4>
+                <h4>UI Testing</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default SectionSkill
